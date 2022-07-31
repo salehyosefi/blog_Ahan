69c9f34874ea0fe32e3f7ea038398e13d8ec6476
Salehyousefi1400 <salehyousefi1400@gmail.com>
2022-07-31 11:42:33 +0430

    update model image

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,20 @@
 from .models import Post
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 def post_list(request):
     posts = Post.objects.all()
+    paginator = Paginator(posts, 2)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
     return render(request, 'blog/post_list.html', {
         'posts': posts
     })
